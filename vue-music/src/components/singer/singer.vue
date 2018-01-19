7f1108d0fd08fd05637f59383ac8cc46620dfb35
Hey`w <583175694@qq.com>
2018-01-19 16:12:18 +0800

    summary

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -5,6 +5,7 @@
 <script type="text/ecmascript-6">
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
@@ -22,6 +23,7 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
+          this.singers = res.data.list
           console.log(this._normalizsSinger(this.singers))
         }
       })
@@ -35,25 +37,37 @@ export default {
       }
       list.forEach((item, index) => {
         if (index < HOT_SINGER_LEN) {
-          map.hot.items.push({
-            id: item.Fsinger_id,
-            name: item.Fsinger_name,
-            avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
-          })
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
         }
         const key = item.Findex
         if (!map[key]) {
           map[key] = {
-            titel: key,
+            title: key,
             item: []
           }
         }
-        map[key].item.push({
-          id: item.Fsinger_id,
-          name: item.Fsinger_name,
-          avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
-        })
+        map[key].item.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
       })
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   }
 }
