435367f518a0afde4ef06119594a50647e5fe68b
Hey`w <583175694@qq.com>
2018-01-23 21:05:31 +0800

    summary

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -10,6 +10,7 @@ import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import Singer from 'common/js/singer'
 import ListView from 'base/listview/listview'
+import {mapMutations} from 'vuex'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
@@ -31,7 +32,6 @@ export default {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
           this.singers = this._normalizsSinger(res.data.list)
-          console.log(this.singers)
         }
       })
     },
@@ -81,7 +81,10 @@ export default {
         path: `/singer/${singer.id}`
       })
       this.setSinger(singer)
-    }
+    },
+    ...mapMutations({
+      setSinger: 'SET_SINGER'
+    })
   }
 }
 
