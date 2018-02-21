4564616fae0268ef2ebdcb3f644c6cdb50231e1c
Hey`w <583175694@qq.com>
2018-02-21 09:40:14 +0800

    summary

--- vue-music/src/api/rank.js
+++ vue-music/src/api/rank.js
@@ -12,3 +12,19 @@ export function getTopList() {
 
   return jsonp(url, data, options)
 }
+
+export function getMusicList(topid) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1,
+    tpl: 3,
+    page: 'detail',
+    type: 'top',
+    topid
+  })
+
+  return jsonp(url, data, options)
+}
