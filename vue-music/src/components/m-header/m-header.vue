2f6098f7581226cfe241ff8fde9ff8b3fa556ae7
Hey`w <583175694@qq.com>
2018-02-26 13:16:12 +0800

    summary

--- vue-music/src/components/m-header/m-header.vue
+++ vue-music/src/components/m-header/m-header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-header">
     <div class="icon"></div>
-    <h1 class="text">Hey ` w</h1>
+    <h1 class="text">Hey丶w</h1>
     <router-link to="/user" class="mine" tag="div">
       <i class="icon-mine"></i>
     </router-link>
